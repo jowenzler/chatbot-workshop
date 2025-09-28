6dd7b2ee8c49bf7b2810f6e22036ef13e56f815a
jowenzler <30584096+jowenzler@users.noreply.github.com>
2025-09-27 19:22:09 -0700

    Changed instructions to LLM to respond as a Kant Expert

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,11 +43,9 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-2.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
-        Respond using a florid but direct tone, typical of an early modernist writer.
-        Keep your answers under 100 words.""",
+        system_prompt="""You are an expert on Kant's Critique of Pure Reason. Using the provided documents, respond with answers about the Critique. Explain concepts in language that a college undergraduate would understand.
+        Whenever possible, include a quote from the provided text.
+        Keep your answers under 200 words.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
     {
